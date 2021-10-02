a8f759cae30e9843555dc0116f86256eb97b191a
Colette <83985997+TheCheekyGit@users.noreply.github.com>
2021-10-02 10:32:16 +0100

    JavaScript-Core-2-Coursework-Week1

--- mandatory/5-groceries.js
+++ mandatory/5-groceries.js
@@ -30,6 +30,15 @@ Exercise 1:
 // Gather all week item names into this array
 let weeklyGroceriesToBuy = [];
 
+for (let key in weeklyMealPlan){
+  weeklyMealPlan[key].forEach((element) => {
+  if (!weeklyGroceriesToBuy.includes(element)) {
+  weeklyGroceriesToBuy.push(element)
+}
+})
+};
+  
+
 /*
 Exercise 2:
   Loop through your list again, but now only collect the weekend items into the weekendGroceriesToBuy array.
